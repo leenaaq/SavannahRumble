aaf51ebfdd5ac7a890990d8deb172a58b7ef0c0d
PGH\qkrru <qkrrudgh30@naver.com>
2025-03-31 14:38:28 +0900

    create project

---
+++ Source/Team06/Team06.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Team06.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team06, "Team06" );
