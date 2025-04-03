44a18a2fba1f033e81f4bc0143e832329e8dcf72
leenaaq <v584731@gmail.com>
2025-04-03 19:25:10 +0900

    add 플레이어베이스 스탯 추가, 스탯 데이터 테이블 추가

--- Source/Team06/Player/Player/PlayerBase.cpp
+++ Source/Team06/Player/Player/PlayerBase.cpp
@@ -1,7 +1,19 @@
 #include "PlayerBase.h"
 
-
 APlayerBase::APlayerBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APlayerBase::UpdateStatsFromDataTable()
+{
+	if (StatsRowHandle.DataTable)
+	{
+		static const FString ContextString(TEXT("Player stats retrieval"));
+		FPlayerStats* Stats = StatsRowHandle.DataTable->FindRow<FPlayerStats>(StatsRowHandle.RowName, ContextString);
+		if (Stats)
+		{
+			PlayerStats = *Stats;
+		}
+	}
 }
