34507ce6f284e6d4bcd810e8607de20e09de45cb
SeongJunKang95 <skyrnt777@gmail.com>
2025-04-14 10:44:56 +0900

    아이템

--- Source/Team06/Player/Player/PlayerBase.h
+++ Source/Team06/Player/Player/PlayerBase.h
@@ -60,7 +60,7 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 	virtual void ValidateEssentialReferences();
-	float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
 
 	// 데이터 테이블 적용
 	void UpdateStatsFromDataTable();
@@ -116,6 +116,9 @@ public:
 	void ActiveRagdoll();
 	void DeactivateActiveRagdoll();
 
+	const FPlayerStats& GetStats() const { return PlayerStats; }
+
+	
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	class UItemManagerComponent* ItemManager;
