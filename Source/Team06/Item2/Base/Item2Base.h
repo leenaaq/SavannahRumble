80c7eba85c66b9b767a7c6aeb587d1012a916bf5
leenaaq <v584731@gmail.com>
2025-04-15 15:30:43 +0900

    Add 원거리 아이템 (폭탄)

--- Source/Team06/Item2/Base/Item2Base.h
+++ Source/Team06/Item2/Base/Item2Base.h
@@ -1,15 +1,77 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item2Base.generated.h"
 
-/**
- * 
- */
-class TEAM06_API Item2Base
+UENUM(BlueprintType)
+enum class EItemState : uint8
 {
+	Flying UMETA(DisplayName = "Flying"),
+	Landed UMETA(DisplayName = "Landed")
+};
+
+UCLASS()
+class TEAM06_API AItem2Base : public AActor
+{
+	GENERATED_BODY()
+
 public:
-	Item2Base();
-	~Item2Base();
+	AItem2Base();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	virtual void OnSpawn();
+
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	virtual void OnCollision(AActor* OtherActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	virtual void TransitionToLanded();
+
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	virtual void ApplyEffect(class APlayerBase* AffectedPlayer);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	virtual void ServerHandleCollision(AActor* OtherActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	virtual void InitializeFromDataTable();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	class UProjectileMovementComponent* ProjectileMovementComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	EItemState CurrentState;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	float Damage;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	float StunDuration;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	float ExplosionDelay;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	float ExplosionRadius;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	float KnockBackMultiplier;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	UDataTable* ItemDataTable;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	FName ItemRowName;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	float SpawnTime;
 };
